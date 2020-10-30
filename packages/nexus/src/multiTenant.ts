078aeb80a8b6d126360807bad2ce907d147969f1
BjoernRave <bjoern.rave@gmail.com>
2020-10-30 16:13:03 +0100

    remove nexus

--- packages/nexus/src/multiTenant.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from 'nexus-plugin-prisma/client'
-import { MultiTenant } from '@prisma-multi-tenant/client'
-
-export const multiTenant = new MultiTenant<PrismaClient>()
--- packages/nexus/src/settings.ts
+++
@@ -1,5 +0,0 @@
-import { Settings } from 'nexus-plugin-prisma/dist/framework/settings'
-
-export interface MultiTenantSettings extends Settings {
-  tenantRouter: (req: Express.Request) => string | Promise<string>
-}
